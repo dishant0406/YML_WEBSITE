d74842b210118237ed8769cdc6d389b80ebbde11
arogya <arogyabichpuria16@gmail.com>
2022-07-29 12:33:42 +0530

    added animations for other blocks

--- components/index/SectionFour.js
+++ components/index/SectionFour.js
@@ -1,39 +1,85 @@
-import React from 'react'
-import background from '../../assets/images/Sec4.png'
-import Image from 'next/image'
-
+import React from "react";
+import background from "../../assets/images/Sec4.png";
+import Image from "next/image";
+import { motion, useScroll } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 function SectionFour() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className='panel h-screen w-screen flex items-end justify-center'>
-      <div className='w-full h-[90%] flex justify-center items-center'>
-        <div className='w-full h-[95%] flex justify-center items-center'>
-          <div className='absolute w-[100vw] h-[85vh] top-[82.5%] overflow-hidden'>
-            <Image className="h-full brightness-[35%]" src={background} layout="responsive" alt='background'></Image>
+    <div
+      ref={ref}
+      className="panel h-screen w-screen flex items-end justify-center"
+    >
+      <div className="w-full h-[90%] flex justify-center items-center">
+        <div className="w-full h-[95%] flex justify-center items-center">
+          <div className="absolute w-[100vw] h-[85vh] top-[82.5%] overflow-hidden">
+            <Image
+              className="h-full brightness-[35%]"
+              src={background}
+              layout="responsive"
+              alt="background"
+            ></Image>
           </div>
-          <div className='z-[4] w-[75%] h-[85%] border-[3.5px] border-[#808080] flex items-center justify-center'>
+          <div className="z-[4] w-[75%] h-[85%] border-[3.5px] border-[#808080] flex items-center justify-center">
+            <div className="h-[80%] w-[80%] flex items-center justify-center flex-col">
+              <div className="h-[40%] w-full flex justify-end items-end">
+                <div className="h-[65%] w-[15%] border-t-[4px] border-white"></div>
+                <motion.div
+                  initial={{ x: 600, opacity: 0 }}
+                  animate={
+                    inView
+                      ? {
+                          x: 0,
+                          opacity: 1,
+                          transition: { duration: 1 },
+                        }
+                      : {
+                          x: 600,
 
-            <div className='h-[80%] w-[80%] flex items-center justify-center flex-col'>
-              <div className='h-[40%] w-full flex justify-end items-end'>
-                <div className='h-[65%] w-[15%] border-t-[4px] border-white'></div>
-                <div className='h-full w-[75%] pl-8 font-proximaRegular text-left text-4xl tracking-[0.7rem] flex items-center'>
+                          opacity: 0,
+                          transition: { duration: 1 },
+                        }
+                  }
+                  className="h-full w-[75%] pl-8 font-proximaRegular text-left text-4xl tracking-[0.7rem] flex items-center"
+                >
                   WE’LL HELP YOU GROW YOUR DIGITAL ASSETS!
-                </div>
+                </motion.div>
               </div>
-              <div className='h-[45%] w-full flex justify-end items-end'>
-                <div className='h-[95%] w-[4%] border-t-[4px] border-yellow-300'></div>
-                <div className='h-full w-[75%] pl-8 font-proximaRegular text-left text-lg'>
-                  When was the last time you looked at your digital statistics and felt, ‘Woah! That’s amazing!’? Our team of experts curate your online footprint through customized digital solutions.
+              <div className="h-[45%] w-full flex justify-end items-end">
+                <div className="h-[95%] w-[4%] border-t-[4px] border-yellow-300"></div>
+                <motion.div
+                  initial={{ x: -600, opacity: 0 }}
+                  animate={
+                    inView
+                      ? {
+                          x: 0,
+                          opacity: 1,
+                          transition: { duration: 1 },
+                        }
+                      : {
+                          x: -600,
 
-                  We build and grow your online presence driven by market data and analytics so you will always be ahead of the digital curve.
-                </div>
+                          opacity: 0,
+                          transition: { duration: 1 },
+                        }
+                  }
+                  className="h-full w-[75%] pl-8 font-proximaRegular text-left text-lg"
+                >
+                  When was the last time you looked at your digital statistics
+                  and felt, ‘Woah! That’s amazing!’? Our team of experts curate
+                  your online footprint through customized digital solutions. We
+                  build and grow your online presence driven by market data and
+                  analytics so you will always be ahead of the digital curve.
+                </motion.div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SectionFour
+export default SectionFour;
--- components/index/SectionThree.js
+++ components/index/SectionThree.js
@@ -1,41 +1,118 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function SectionThree() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className='panel h-screen w-screen flex items-end justify-center'>
-        <div className='h-[90%] w-[75%]'>
-            <div className='h-[25%] w-full flex flex-col items-center justify-between'>
-                <div className='w-full h-[35%] text-5xl font-proximaRegular tracking-[0.7rem]'>OUR CLIENTS</div>
-                <div className='w-[10%] h-0 border-t-[3px] border-yellow-300'></div>
-                <div className='w-[60%] h-[50%] text-xl font-proximaRegular flex justify-center items-center'>
-                    Lörem ipsum ter hunav dosm, berade. Döplagen ponera med ljudaffisch i kidiktig plus syrur, depreligen. Deplaling nybel öv då Gretaeffekten multin eftersom till favis. 
-                </div>
-            </div>
-            <div className='h-[70%] w-full flex justify-center items-center'>
-                <div className='h-[90%] w-[90%] flex justify-between items-center'>
-                    <div className='h-full w-[20%] flex flex-col justify-around items-center'>
-                        <div className='h-[48%] w-[95%] bg-[#c3c3c3]'></div>
-                        <div className='h-[48%] w-[95%] bg-[#a3a3a3]'></div>
-                    </div>
-                    <div className='h-full w-[20%] flex flex-col justify-around items-center'>
-                        <div className='h-[98%] w-[95%] bg-[#d3d2d2]'></div>
-                    </div>
-                    <div className='h-full w-[40%] flex flex-col justify-around items-center'>
-                        <div className='h-[48%] w-[97%] bg-[#eeeeee]'></div>
-                        <div className='h-[48%] w-[97%] flex justify-between items-center'>
-                            <div className='w-[49%] h-full bg-[#858585]'></div>
-                            <div className='w-[49%] h-full bg-[#c3c3c3]'></div>
-                        </div>
-                    </div>
-                    <div className='h-full w-[20%] flex flex-col justify-around items-center'>
-                        <div className='h-[48%] w-[95%] bg-[#c3c3c3]'></div>
-                        <div className='h-[48%] w-[95%] bg-[#a3a3a3]'></div>
-                    </div>
-                </div>
-            </div>
+    <div
+      ref={ref}
+      className="panel h-screen w-screen flex items-end justify-center"
+    >
+      <div className="h-[90%] w-[75%]">
+        <div className="h-[25%] w-full flex flex-col items-center justify-between">
+          <motion.div
+            initial={{ x: -300, opacity: 0 }}
+            animate={
+              inView
+                ? {
+                    x: 0,
+                    opacity: 1,
+                    transition: { duration: 1 },
+                  }
+                : { x: -300, opacity: 0, transition: { duration: 1 } }
+            }
+            className="w-full h-[35%] text-5xl font-proximaRegular tracking-[0.7rem]"
+          >
+            OUR CLIENTS
+          </motion.div>
+          <div className="w-[10%] h-0 border-t-[3px] border-yellow-300"></div>
+          <motion.div
+            initial={{ x: 300, opacity: 0 }}
+            animate={
+              inView
+                ? {
+                    x: 0,
+                    opacity: 1,
+                    transition: { duration: 1 },
+                  }
+                : { x: 300, opacity: 0, transition: { duration: 1 } }
+            }
+            className="w-[60%] h-[50%] text-xl font-proximaRegular flex justify-center items-center"
+          >
+            Lörem ipsum ter hunav dosm, berade. Döplagen ponera med ljudaffisch
+            i kidiktig plus syrur, depreligen. Deplaling nybel öv då
+            Gretaeffekten multin eftersom till favis.
+          </motion.div>
         </div>
+        <div className="h-[70%] w-full flex justify-center items-center">
+          <div className="h-[90%] w-[90%] flex justify-between items-center">
+            <div className="h-full w-[20%] flex flex-col justify-around items-center">
+              <div className="h-[48%] w-[95%] bg-[#c3c3c3]"></div>
+              <div className="h-[48%] w-[95%] bg-[#a3a3a3]"></div>
+            </div>
+            <div className="h-full w-[20%] flex flex-col justify-around items-center">
+              <motion.div
+                initial={{ x: -600, opacity: 0 }}
+                animate={
+                  inView
+                    ? {
+                        x: 0,
+                        opacity: 1,
+                        transition: { duration: 1 },
+                      }
+                    : { x: -600, opacity: 0, transition: { duration: 1 } }
+                }
+                className="h-[98%] w-[95%] bg-[#d3d2d2]"
+              ></motion.div>
+            </div>
+            <div className="h-full w-[40%] flex flex-col justify-around items-center">
+              <motion.div
+                initial={{ y: -600, opacity: 0 }}
+                animate={
+                  inView
+                    ? {
+                        y: 0,
+                        opacity: 1,
+                        transition: { duration: 1 },
+                      }
+                    : { y: -600, opacity: 0, transition: { duration: 1 } }
+                }
+                className="h-[48%] w-[97%] bg-[#eeeeee]"
+              ></motion.div>
+              <div className="h-[48%] w-[97%] flex justify-between items-center">
+                <motion.div
+                  initial={{ x: -600, opacity: 0 }}
+                  animate={
+                    inView
+                      ? {
+                          x: 0,
+                          opacity: 1,
+                          transition: { duration: 1 },
+                        }
+                      : {
+                          x: -600,
+
+                          opacity: 0,
+                          transition: { duration: 1 },
+                        }
+                  }
+                  className="w-[49%] h-full bg-[#858585]"
+                ></motion.div>
+                <div className="w-[49%] h-full bg-[#c3c3c3]"></div>
+              </div>
+            </div>
+            <div className="h-full w-[20%] flex flex-col justify-around items-center">
+              <div className="h-[48%] w-[95%] bg-[#c3c3c3]"></div>
+              <div className="h-[48%] w-[95%] bg-[#a3a3a3]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SectionThree
+export default SectionThree;
